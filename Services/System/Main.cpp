0d5b216e2e616f4394b5911faa45636444aab869
Saullo Bretas Silva <saullo.bretas55@gmail.com>
2023-02-15 23:51:50 -0300

    System: Added initial files

---
+++ Services/System/Main.cpp
@@ -0,0 +1,26 @@
+/**
+ * @file Services/System/Main.cpp
+ * @author Saullo Bretas Silva (saullo.bretas55@gmail.com)
+ * @brief System service entrypoint
+ * @version 0.1
+ * @date 2023-15-02
+ *
+ * @copyright Ample Operating System
+ * Copyright (C) 2023 Saullo Bretas Silva
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+int main() { return 0; }
